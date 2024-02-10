65f0da96d84169b2905a30d1875b2e21a7671833
NZYK <nozuyukiwork@gmail.com>
2024-02-10 16:13:15 +0900

    セッションの実装とテスト

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+//セッションをインポート（サーバー側にセッションデータを持たせる）
+const session = require("express-session");
+const memoryStore = new session.MemoryStore;
+const sess = {
+    secret: "NZYKsecret",
+    resave: false, //セッション内容に変更がないときにはデータをリセーブしない
+    saveUninitialized: true, //初期化されていないセッションも保存するか:true
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}, //保持期間 1000ms * 60sec * 60min * 24hours * 30 days
+    store: memoryStore //セッションの保存先
+};
+//本番環境に移したときのみ実行する設定
+if (app.get("env") === "production"){
+    app.set("trust proxy", 1);//プロキシサーバーから1番目をクライアントのIPとして扱う
+    sess.cookie.secure = true;//HTTPSによるアクセス時のみcookieを有効化する
+}
 //ほぼjinja2のテンプレートエンジンをインポート
 const jinja = require("nunjucks");
 jinja.configure("./static/template", {
@@ -11,7 +26,8 @@ jinja.configure("./static/template", {
 app.set("views","./static/template")
 app.set("view engine","html");
 app.set("json spaces", 2);
-
+//セッションミドルウェア
+app.use(session(sess));
 //アクセスログミドルウェア
 app.use(function (req, res, next) {
     const reqUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
@@ -57,12 +73,32 @@ app.post("/create",function (req, res){
     res.render("create.html");
 });
 
+//セッション利用のテスト
+app.get("/session", function (req, res){
+    console.log(req.session);
+    if (req.session.counter){
+        req.session.counter++;
+    }else{
+        req.session.counter = 1;
+    }
+    res.send(`<html><body>
+            <p>${req.session.counter}</p>
+            <p>domain:${req.session.cookie.domain}</p>
+            <p>expires:${req.session.cookie.expires}</p>
+            <p>httpOnly:${req.session.cookie.httpOnly}</p>
+            <p>maxAge:${req.session.cookie.maxAge}</p>
+            <p>originalMaxAge:${req.session.cookie.originalMaxAge}</p>
+            <p>path:${req.session.cookie.path}</p>
+            <p>sameSite:${req.session.cookie.sameSite}</p>
+            <p>secure:${req.session.cookie.secure}</p>
+            <p>signed:${req.session.cookie.signed}</p>
+    </body></html>`);
+});
 
 //ルーティングの404エラーハンドリング
 app.use(function (req, res, next) {
     res.status(404).send('ERROR 404 not found');
 });
-
 
 //起動
 app.listen(port, () => {
